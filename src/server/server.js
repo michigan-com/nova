df15b6cc7aac4889fd24118aae0ad5605a1dafa2
Mike Varano <mikevee313@gmail.com>
2016-04-11 17:07:29 -0400

    tmp

--- src/server/server.js
+++ src/server/server.js
@@ -3,19 +3,39 @@
 import debug from 'debug';
 var logger = debug('app:status');
 
-import app from './app';
+import { createApp } from './app';
 
-var port = normalizePort(process.env.NODE_PORT || '3000');
-app.set('port', port);
+// Connect to the db then start the app
+async function startServer() {
+  let db = await dbConnect(process.env.DB_URI);
 
-logger(`Environment: ${app.get('env')}`);
-var server = app.listen(port, '0.0.0.0', function(err) {
-  if (err) throw new Error(err);
+  var app = createApp(db);
+  var port = normalizePort(process.env.NODE_PORT || '3000');
+  app.set('port', port);
 
-  let host = this.address();
-  logger(`Started on ${host.address}:${host.port}`);
-});
-export default server;
+  // Create the app
+  var app = createApp(db, true);
+  var port = normalizePort(process.env.NODE_PORT || '3000');
+  app.set('port', port);
+
+  logger(`[SERVER] Environment: ${app.get('env')}`);
+  var server = app.listen(port, '0.0.0.0', function(err) {
+    if (err) throw new Error(err);
+
+    let host = this.address();
+    logger(`[SERVER] Started on ${host.address}:${host.port}`);
+  });
+
+  server.on('close', function() {
+    logger("[SERVER] Closed nodejs application ...");
+    db.close();
+  });
+
+  process.on('SIGTERM', function () {
+    db.close();
+    server.close();
+  });
+}
 
 function normalizePort(val) {
   var port = parseInt(val, 10);
@@ -24,3 +44,4 @@ function normalizePort(val) {
   return false;
 }
 
+startServer();
