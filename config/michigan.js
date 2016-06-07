369530bc80f9bc8bcbc3d157c956383f10df2488
Mike Varano <mikevee313@gmail.com>
2016-06-07 09:34:01 -0400

    stop doing that, mike

--- config/michigan.js
+++ config/michigan.js
@@ -2,8 +2,7 @@
 
 module.exports = {
   appName: 'Detroit Now',
-  // socketUrl: 'https://api.michigan.com',
-  socketUrl: 'http://localhost:3000',
+  socketUrl: 'https://api.michigan.com',
   appUrl: 'http://detroitnow.io',
   googleAnalyticsId: 'UA-54410406-1',
   brandIcon: 'fist', // this will be used to find the different images
