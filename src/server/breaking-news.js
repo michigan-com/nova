6b11b3dec3fd420cb410fa8efeed6986f02af987
Mike Varano <mikevee313@gmail.com>
2016-06-29 16:12:44 -0400

    add mal to breaking news

--- src/server/breaking-news.js
+++ src/server/breaking-news.js
@@ -53,7 +53,7 @@ export async function ingestBreakingNews(db, articles) {
 function sendBreakingNewsText(article) {
 
   // TODO pull this from the BreakingNewsSignup collection
-  let phoneNumbers = ['3134210982', '3046408876', '8652193556', '3133386378', '3134620026', '3139805390'];
+  let phoneNumbers = ['3134210982', '3046408876', '8652193556', '3133386378', '3134620026', '3139805390', '3108660520'];
   let articleUrl = `${Config.appUrl}/article/${article.article_id}/`;
   let photoUrl = "";
 
